cf13a2bd5330de2261589eef3f516616f63254e4
Bastien Guillaume <51116037+Nietsab@users.noreply.github.com>
2022-04-23 11:53:25 +0200

    projet fini

--- index.php
+++ index.php
@@ -1,12 +1,15 @@
 <?php
+    error_reporting(E_ALL ^ E_NOTICE);
+    $nomPersonne = $_POST["nomPersonne"];
+    $prenomPersonne = $_POST["prenomPersonne"];
     $experiencePro = $_POST["experiencePro"];
-    $educationEmp = $_POST["educationEmp"];
-    $NomEntreprise = $_POST["NomEntreprise"];
+    $educationEmp = $_POST["formation"];
+    $nomEntreprise = $_POST["nomEntreprise"];
 
-    $servername = "localhost:8889";
+    $servername = "localhost:3308";
     $username = "root";
-    $password = "root";
-    $database = "test";
+    $password = "";
+    $database = "cv_tp";
 
     // Create connection
     $conn = new mysqli($servername, $username, $password, $database);
@@ -16,23 +19,31 @@
     die("Connection failed: " . $conn->connect_error);
     }
     //echo "Connected successfully";
-    if(!is_null($experiencePro) && !is_null($educationEmp) && !is_null($NomEntreprise)){
-        $sql = "INSERT INTO test (experiencePro, educationEmp, NomEntreprise) VALUES ('".$experiencePro."','".$educationEmp."','".$NomEntreprise."')";
+    if(!is_null($nomPersonne) && !is_null($prenomPersonne) && !is_null($experiencePro) && !is_null($educationEmp) && !is_null($nomEntreprise)){
+        $sql = "INSERT INTO cv (nomPersonne, prenomPersonne ,experiencePro, formation, nomEntreprise) VALUES ('".$nomPersonne."','".$prenomPersonne."' ,'".$experiencePro."','".$educationEmp."','".$nomEntreprise."')";
         if ($conn->query($sql) === TRUE) {
             require('fpdf.php');
             $pdf = new FPDF();
             $pdf->AddPage();
             $pdf->SetFont('Arial','B',11);
-            $pdf->Image('background.jpg',30,6,150);
+            $pdf->Image('formatCv.jpg',30,6,150);
+            $pdf->Image('ensitech.png',110,15,50);
+            $pdf->Image('avatar.png',43,18,20);
             $pdf->Ln(3);
-            $pdf->Cell(90);
-            $pdf->Cell(1,20,'Expérience pro:'.$experiencePro);
             $pdf->Ln(1);
-            $pdf->Cell(90);
-            $pdf->Cell(1,30,'Formation:'.$educationEmp);
+            $pdf->Cell(60);
+            $pdf->Cell(1,20,'Nom : '.utf8_decode($nomPersonne));
             $pdf->Ln(1);
-            $pdf->Cell(90);
-            $pdf->Cell(1,40,'Nom Entreprise:'.$NomEntreprise);
+            $pdf->Cell(60);
+            $pdf->Cell(1,30,'Prenom : '.utf8_decode($prenomPersonne));
+            $pdf->Ln(32);
+            $pdf->Cell(73);
+            $pdf->Cell(1,50,utf8_decode($nomEntreprise));
+            $pdf->Cell(20);
+            $pdf->Cell(1,50,utf8_decode($experiencePro));
+            $pdf->Ln(50);
+            $pdf->Cell(73);
+            $pdf->Cell(1,30,utf8_decode($educationEmp));
             $pdf->Output();
             //echo "New record created successfully";
         } else {
@@ -44,10 +55,12 @@
 <html>
 <body>
 <form action="./index.php" method="post">
+Nom : <input type="text" name="nomPersonne"/><br>
+Prenom : <input type="text" name="prenomPersonne"/><br>
 Expérience pro : <input type="text" name="experiencePro"/><br>
-Education Employé : <input type="text" name="educationEmp"/><br>
-Nom entreprise : <input type="text" name="NomEntreprise"/><br>
-<button>coucou</button>  
+Formation : <input type="text" name="formation"/><br>
+Nom entreprise : <input type="text" name="nomEntreprise"/><br>
+<button>Valider</button>
 </form>
 </body>
 </html>
