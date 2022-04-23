f467641b6e14a5d2f05cfc5c5e53a4c14b1c0cb6
Nietsab <bastienguillaume78@gmail.com>
2022-04-23 10:59:32 +0200

    init

---
+++ .history/index_20220406164903.php
@@ -0,0 +1,54 @@
+<?php
+    error_reporting(E_ERROR | E_PARSE);
+
+    $nom = $_POST["experiencePro"];
+    $prenom = $_POST["educationEmp"];
+    $mail = $_POST["NomEntreprise"];
+
+    $servername = "localhost:8889";
+    $username = "root";
+    $password = "root";
+    $database = "test";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $database);
+
+    // Check connection
+    if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+    }
+    //echo "Connected successfully";
+    if(!is_null($nom) && !is_null($prenom) && !is_null($mail)){
+        $sql = "INSERT INTO test (experiencePro, educationEmp) VALUES ('".$nom."','".$prenom."','".$mail."')";
+        if ($conn->query($sql) === TRUE) {
+            require('fpdf.php');
+            $pdf = new FPDF();
+            $pdf->AddPage();
+            $pdf->SetFont('Arial','B',11);
+            $pdf->Image('background.jpg',30,6,150);
+            $pdf->Ln(3);
+            $pdf->Cell(90);
+            $pdf->Cell(1,20,'Nom:'.$nom);
+            $pdf->Ln(1);
+            $pdf->Cell(90);
+            $pdf->Cell(1,30,'Prenom:'.$prenom);
+            $pdf->Ln(1);
+            $pdf->Cell(90);
+            $pdf->Cell(1,40,'Mail:'.$mail);
+            $pdf->Output();
+            //echo "New record created successfully";
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+    }
+?>
+<!DOCTYPE html>
+<html>
+<body>
+<form action="./index.php" method="post">
+Expérience pro : <input type="text" name="experiencePro"/><br>
+Education Employé : <input type="text" name="educationEmp"/><br>
+Nom entreprise : <input type="text" name="NomEntreprise"/><br>
+</form>
+</body>
+</html>
